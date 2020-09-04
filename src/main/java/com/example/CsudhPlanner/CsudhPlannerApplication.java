d8612bb88657185c1889228dc09d97ad002103f3
Mjkli <Mjkli@users.noreply.github.com>
2020-09-04 10:48:11 -0700

    Initial commit

---
+++ src/main/java/com/example/CsudhPlanner/CsudhPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CsudhPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsudhPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsudhPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CsudhPlanner/CsudhPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CsudhPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsudhPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
