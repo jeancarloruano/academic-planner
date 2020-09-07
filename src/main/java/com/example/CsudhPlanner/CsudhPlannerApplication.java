4ba33857a975fc99e9635640d812831da83afd5d
Mjkli <Mjkli@users.noreply.github.com>
2020-09-07 13:03:56 -0700

    Learning how to use Spring. Following this course on YT: https://www.youtube.com/watch?v=vtPkZShrvXQ

--- src/main/java/com/example/CsudhPlanner/CsudhPlannerApplication.java
+++ src/main/java/com/example/CsudhPlanner/CsudhPlannerApplication.java
@@ -8,6 +8,7 @@ public class CsudhPlannerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CsudhPlannerApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/com/example/CsudhPlanner/dao/personDao.java
@@ -0,0 +1,29 @@
+package com.example.CsudhPlanner.dao;
+
+import com.example.CsudhPlanner.model.Person;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface personDao {
+
+    //Inserst person into database with a set UUID
+    int insertPerson(UUID id, Person person);
+
+
+    //Inserst person into data base with a random UUID
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id,person);
+    }
+
+    List<Person> selectAllPeople();
+
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersoonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
+
+}
